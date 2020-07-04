2eb06991bbe9d03536da576c2cd64e11533babb8
Dale Margel <dale.margel@syban.net>
2020-07-04 13:43:57 -0600

    Complete header ex underlines (still bugs)

--- demo/markdown/markdown.js
+++ demo/markdown/markdown.js
@@ -62,53 +62,6 @@ let init=rule`''`.on(()=>push(""));
 	line.set(rule`${init}{${other}|${step}}['\n']`);
 }
 
-test(pars(line,"abc"),"abc");
-test(pars(line,"abc\n"),"abc");
-test(pars(line,"a*b*c"),"a<i>b</i>c");
-test(pars(line,"a**b**c"),"a<b>b</b>c");
-test(pars(line,"a~b~c"),"a<s>b</s>c");
-
-test(pars(line,"**abc**"),"<b>abc</b>");
-test(pars(line,"**a*b*c**"),"<b>a<i>b</i>c</b>");
-test(pars(line,"**a_b_c**"),"<b>a<i>b</i>c</b>");
-test(pars(line,"**a~b~c**"),"<b>a<s>b</s>c</b>");
-test(pars(line,"**a**b**c**"),"<b>a</b>b<b>c</b>");
-test(pars(line,"**a__b__c**"),"<b>a_<i>b</i>_c</b>");
-test(pars(line,"**abc*"),"*<i>abc</i>");
-test(pars(line,"**abc"),"**abc");
-
-test(pars(line,"__abc__"),"<b>abc</b>");
-test(pars(line,"__a*b*c__"),"<b>a<i>b</i>c</b>");
-test(pars(line,"__a_b_c__"),"<b>a<i>b</i>c</b>");
-test(pars(line,"__a**b**c__"),"<b>a*<i>b</i>*c</b>");
-test(pars(line,"__a__b__c__"),"<b>a</b>b<b>c</b>");
-test(pars(line,"__abc_"),"_<i>abc</i>");
-test(pars(line,"__abc"),"__abc");
-
-test(pars(line,"*abc*"),"<i>abc</i>");
-test(pars(line,"*a**b**c*"),"<i>a<b>b</b>c</i>");
-test(pars(line,"*a__b__c*"),"<i>a<b>b</b>c</i>");
-test(pars(line,"*a~b~c*"),"<i>a<s>b</s>c</i>");
-test(pars(line,"*a*b*c*"),"<i>a</i>b<i>c</i>");
-test(pars(line,"*a_b_c*"),"<i>a_b_c</i>");
-test(pars(line,"*abc**"),"<i>abc</i>*");
-test(pars(line,"*abc"),"*abc");
-
-test(pars(line,"_abc_"),"<i>abc</i>");
-test(pars(line,"_a**b**c_"),"<i>a<b>b</b>c</i>");
-test(pars(line,"_a__b__c_"),"<i>a<b>b</b>c</i>");
-test(pars(line,"_a~b~c_"),"<i>a<s>b</s>c</i>");
-test(pars(line,"_a*b*c_"),"<i>a*b*c</i>");
-test(pars(line,"_a_b_c_"),"<i>a</i>b<i>c</i>");
-test(pars(line,"_abc__"),"<i>abc</i>_");
-
-test(pars(line,"~abc~"),"<s>abc</s>");
-test(pars(line,"~a**b**c~"),"<s>a<b>b</b>c</s>");
-test(pars(line,"~a__b__c~"),"<s>a<b>b</b>c</s>");
-test(pars(line,"~a*b*c~"),"<s>a<i>b</i>c</s>");
-test(pars(line,"~a_b_c~"),"<s>a<i>b</i>c</s>");
-test(pars(line,"~a~b~c~"),"<s>a</s>b<s>c</s>");
-
 let header=defer();
 {
 	let n=0;
@@ -119,17 +72,75 @@ let header=defer();
 	header.set(rule`!'#######'${head}`);
 }
 
-test(pars(header,"# abc"),"<h1>abc</h1>");
-test(pars(header,"## abc"),"<h2>abc</h2>");
-test(pars(header,"### abc"),"<h3>abc</h3>");
-test(pars(header,"#### abc"),"<h4>abc</h4>");
-test(pars(header,"##### abc"),"<h5>abc</h5>");
-test(pars(header,"###### abc"),"<h6>abc</h6>");
-test(pars(header,"####### abc"),false);
-test(pars(header,"#     abc"),"<h1>abc</h1>");
+// TODO: fix this (underline headers)
+//let head1=rule`${line}('=':3..)'='`.on(()=>push(`<h1>${pop()}</h1>`));
+//let head2=rule`${line}('-':3..)'\n'`.on(()=>push(`<h2>${pop()}</h2>`));
+//test(pars(head1,"abc\n==="),"<h1>abc</h1>");
+//test(pars(head2,"abc\n---"),"<h1>abc</h1>");
 
-//let h1=rule`${text}'=':1..'\n'`.on(()=>write(`<h1>#{pop()}<h1>\n`));
-//let h2=rule`${text}'-':1..'\n'`.on(()=>write(`<h2>#{pop()}<h2>\n`));
+let any=rule`${header}|${line}`
+
+test(pars(any,"abc"),"abc");
+test(pars(any,"abc\n"),"abc");
+test(pars(any,"a*b*c"),"a<i>b</i>c");
+test(pars(any,"a**b**c"),"a<b>b</b>c");
+test(pars(any,"a~b~c"),"a<s>b</s>c");
+
+test(pars(any,"**abc**"),"<b>abc</b>");
+test(pars(any,"**a*b*c**"),"<b>a<i>b</i>c</b>");
+test(pars(any,"**a_b_c**"),"<b>a<i>b</i>c</b>");
+test(pars(any,"**a~b~c**"),"<b>a<s>b</s>c</b>");
+test(pars(any,"**a**b**c**"),"<b>a</b>b<b>c</b>");
+test(pars(any,"**a__b__c**"),"<b>a_<i>b</i>_c</b>");
+test(pars(any,"**abc*"),"*<i>abc</i>");
+test(pars(any,"**abc"),"**abc");
+
+test(pars(any,"__abc__"),"<b>abc</b>");
+test(pars(any,"__a*b*c__"),"<b>a<i>b</i>c</b>");
+test(pars(any,"__a_b_c__"),"<b>a<i>b</i>c</b>");
+test(pars(any,"__a**b**c__"),"<b>a*<i>b</i>*c</b>");
+test(pars(any,"__a__b__c__"),"<b>a</b>b<b>c</b>");
+test(pars(any,"__abc_"),"_<i>abc</i>");
+test(pars(any,"__abc"),"__abc");
+
+test(pars(any,"*abc*"),"<i>abc</i>");
+test(pars(any,"*a**b**c*"),"<i>a<b>b</b>c</i>");
+test(pars(any,"*a__b__c*"),"<i>a<b>b</b>c</i>");
+test(pars(any,"*a~b~c*"),"<i>a<s>b</s>c</i>");
+test(pars(any,"*a*b*c*"),"<i>a</i>b<i>c</i>");
+test(pars(any,"*a_b_c*"),"<i>a_b_c</i>");
+test(pars(any,"*abc**"),"<i>abc</i>*");
+test(pars(any,"*abc"),"*abc");
+
+test(pars(any,"_abc_"),"<i>abc</i>");
+test(pars(any,"_a**b**c_"),"<i>a<b>b</b>c</i>");
+test(pars(any,"_a__b__c_"),"<i>a<b>b</b>c</i>");
+test(pars(any,"_a~b~c_"),"<i>a<s>b</s>c</i>");
+test(pars(any,"_a*b*c_"),"<i>a*b*c</i>");
+test(pars(any,"_a_b_c_"),"<i>a</i>b<i>c</i>");
+test(pars(any,"_abc__"),"<i>abc</i>_");
+
+test(pars(any,"~abc~"),"<s>abc</s>");
+test(pars(any,"~a**b**c~"),"<s>a<b>b</b>c</s>");
+test(pars(any,"~a__b__c~"),"<s>a<b>b</b>c</s>");
+test(pars(any,"~a*b*c~"),"<s>a<i>b</i>c</s>");
+test(pars(any,"~a_b_c~"),"<s>a<i>b</i>c</s>");
+test(pars(any,"~a~b~c~"),"<s>a</s>b<s>c</s>");
+
+test(pars(any,"# abc"),"<h1>abc</h1>");
+test(pars(any,"## abc"),"<h2>abc</h2>");
+test(pars(any,"### abc"),"<h3>abc</h3>");
+test(pars(any,"#### abc"),"<h4>abc</h4>");
+test(pars(any,"##### abc"),"<h5>abc</h5>");
+test(pars(any,"###### abc"),"<h6>abc</h6>");
+test(pars(any,"####### abc"),"####### abc");
+test(pars(any,"#     abc"),"<h1>abc</h1>");
+
+test(pars(any,"# **abc**"),"<h1><b>abc</b></h1>");
+test(pars(any,"# __abc__"),"<h1><b>abc</b></h1>");
+test(pars(any,"# *abc*"),"<h1><i>abc</i></h1>");
+test(pars(any,"# _abc_"),"<h1><i>abc</i></h1>");
+test(pars(any,"# ~abc~"),"<h1><s>abc</s></h1>");
 
 console.log("End test");
 if(errs) console.log(`There were ${errs} errors`)
